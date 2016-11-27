a41f9ea487206decbe50f68136b87f0f2f284cd6
Peter Westmacott <peter.westmacott@gmail.com>
2016-11-27 20:48:23 +0000

    Add prisoners dilemma. Disable wishlist.

---
+++ non-games/prisoners/deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cp ../prisoners/resources/public/css/style.css \
+   ./non-games/prisoners/css/style.css
+
+cp ../prisoners/resources/public/index.html \
+   ./non-games/prisoners/index.html
+
+cp ../prisoners/resources/public/js/compiled/prisoners.js \
+   ./non-games/prisoners/js/compiled/prisoners.js
